41a15205fde5bed59319178f6caa02785ca14097
tbitw2549 <yonshi29@gmail.com>
2024-11-08 23:54:06 +0200

    fix: avoid backtrack by keeping second last char

--- packages/nuxt/src/components/plugins/delayed-hydration.ts
+++ packages/nuxt/src/components/plugins/delayed-hydration.ts
@@ -4,7 +4,7 @@ import MagicString from 'magic-string'
 import { isVue } from '../../core/utils'
 
 export const DelayedHydrationPlugin = (options: { sourcemap: boolean }) => createUnplugin(() => {
-  const DELAYED_HYDRATION_RE = /<(?:Lazy([A-Z]\w*)|lazy-[\w-]+)\b([^>]+)hydrate:(\w+)(="([^"]+)")?([^>]*(?:(?<=\/)|>[\s\S]+?<\/Lazy[A-Z]\w*|lazy-[\w-]+))>/g
+  const DELAYED_HYDRATION_RE = /<(?:Lazy([A-Z]\w*)|lazy-([\w-]+))\b([^>]+)hydrate:(\w+)(="([^"]+)")?([ />])([^>]*(?:(?<=\/)|>[\s\S]+?<\/Lazy[A-Z]\w*|lazy-[\w-]+))>/g
   const types = new Map([['time', 'Time'], ['promise', 'Promise'], ['if', 'If'], ['event', 'Event'], ['visible', 'Visible'], ['media', 'Media'], ['never', 'Never'], ['idle', 'Idle']])
   const correctVals = new Map([['time', 'number'], ['promise', 'Promise'], ['event', 'string | string[]'], ['visible', 'IntersectionObserverInit'], ['media', 'string'], ['idle', 'number']])
   return {
@@ -15,12 +15,12 @@ export const DelayedHydrationPlugin = (options: { sourcemap: boolean }) => creat
     },
     transform (code, id) {
       const s = new MagicString(code)
-      s.replace(DELAYED_HYDRATION_RE, (_, comp, pre, type, selfOrValue, val, post) => {
+      s.replace(DELAYED_HYDRATION_RE, (_, comp, pre, type, selfOrValue, val, nearEndChar, post) => {
         const shouldDefault = type !== 'if' && (val === 'true' || val === 'false')
         if (!types.has(type)) { logger.warn(`Unexpected hydration strategy \`${type}\` when parsing \`${_}\` in \`${id}\`, this will default to visibility. For custom strategies, please use \`v-hydrate:if\` in stead.`) }
         if (type === 'never' && selfOrValue) { logger.warn('`hydrate:never` does not accept any value. It is meant to be used as is, and the value will not affect its runtime behavior.') }
         if (shouldDefault) { logger.warn(`Invalid value \`${val}\` for \`hydrate:${type}\` when parsing \`${_}\` in \`${id}\`. The prop is not meant to be assigned a boolean, but used as is or given ${type === 'never' ? 'no value' : `a value of type \`${correctVals.get(type) ?? 'unknown'}\``}. This will not affect runtime behavior and the defaults will be used in stead.`) }
-        return `<Lazy${types.get(type) ?? 'Visible'}${comp}${pre}${type == 'never' || shouldDefault ? '' : `:hydrate="${val}"`}${post}>`
+        return `<Lazy${types.get(type) ?? 'Visible'}${comp}${pre}${type == 'never' || shouldDefault ? '' : `:hydrate="${val}"`}${nearEndChar}${post}>`
       })
       if (s.hasChanged()) {
         return {
